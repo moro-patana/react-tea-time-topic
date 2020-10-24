043fdb9c98dafc64b0daf2696962e0baf90af086
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-10-24 08:28:31 +0300

    imported the formInput component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import TopicList from "./TopicList.js"
+import FormInput from "./formInput.js"
 const url = "https://gist.githubusercontent.com/Pinois/93afbc4a061352a0c70331ca4a16bb99/raw/6da767327041de13693181c2cb09459b0a3657a1/topics.json"
 
 function App() {
@@ -20,7 +21,10 @@ function App() {
 
     return (
        <div>
-           {topics.map((topic) => (<TopicList key={topic.id} topic={topic}/>))}
+            <FormInput/>
+       	<div>
+       	    {topics.map((topic) => (<TopicList key={topic.id} topic={topic}/>))}
+       	</div>
        </div>
     )
 }
