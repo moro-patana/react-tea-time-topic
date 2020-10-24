c4481e6806bd776e7cc343409adca4ce97037328
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-10-24 11:46:53 +0300

    put the function of the undiscussed topics inside

---
+++ undiscussedTopics.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react"
+import archieveIcon from "./img/archieve-icon.svg"
+import voteIcon from "./img/vote-icon.svg"
+import unvoteIcon from "./img/unvote-icon.svg"
+
+
+function UndiscussedTopics({topic}) {
+    const [upvotedCount, setUpvotedCount] = useState(topic.upvotes)
+    const [downvotedCount, setDownvotedCount] = useState(topic.downvotes)
+
+    return (
+        <div className="container">
+        <div className="article">
+            <p>{topic.title}</p>
+           <button className="archieve"><img className="icon" src={archieveIcon}/></button>
+       </div>
+       <div className="vote">
+           <div className="upvote">
+               <p>{upvotedCount}</p>
+               <button className="btn vote-btn" onClick={() => setUpvotedCount(upvotedCount + 1)}><img className="icon" src={voteIcon}/></button>
+           </div>
+           <div className="count-vote">
+               <p>{downvotedCount}</p>
+               <button className="btn unvote-btn" onClick={() => setDownvotedCount(downvotedCount + 1)}><img className="icon" src={unvoteIcon}/></button>
+           </div>
+       </div>
+   </div>
+
+    )
+}
+export default UndiscussedTopics;
